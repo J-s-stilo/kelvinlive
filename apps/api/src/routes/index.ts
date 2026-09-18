bbdade09c984da72c8b9ad4c5991a106511387db
J-s-stilo <delightedinternationaldelivery@gmail.com>
2026-09-18 22:36:04 +0100

    Update index.ts

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,1 +1,9 @@
+import { Router, type IRouter } from "express";
 
+import creatorRouter from "./creator";
+
+const router: IRouter = Router();
+
+router.use("/creator", creatorRouter);
+
+export default router;
